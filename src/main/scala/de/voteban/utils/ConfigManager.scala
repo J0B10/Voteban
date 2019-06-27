9bdf27935e03b0ef01d28e203c938113edf65f53
Jonas <joblo.2213@gmail.com>
2019-06-27 21:46:34 +0200

    Implement config saving

--- src/main/scala/de/voteban/utils/ConfigManager.scala
+++ src/main/scala/de/voteban/utils/ConfigManager.scala
@@ -1,7 +1,10 @@
 package de.voteban.utils
 
+import java.io.ByteArrayOutputStream
+
 import de.voteban.VotebanBot
-import net.dv8tion.jda.core.Permission
+import de.voteban.config.XMLConfigurationService
+import net.dv8tion.jda.core.{MessageBuilder, Permission}
 import net.dv8tion.jda.core.entities.{Message, User}
 import net.dv8tion.jda.core.events.Event
 import net.dv8tion.jda.core.events.message.priv.PrivateMessageReceivedEvent
@@ -9,18 +12,32 @@ import net.dv8tion.jda.core.hooks.EventListener
 
 import scala.jdk.CollectionConverters._
 
+/**
+  * Handles up & downloading of the config files through private messages
+  * @param bot instance of the bot providing access to jda and the config service
+  */
 class ConfigManager(private val bot: VotebanBot) extends EventListener {
 
+  /**
+    * Called by JDA on any event
+    * @param event any event fired by jda
+    */
   override def onEvent(event: Event): Unit = {
     event match {
       case e: PrivateMessageReceivedEvent => onPM(e)
+      case _ => //Default, do nothing
     }
   }
 
+  /**
+    * Called if a private message is received from any user
+    *
+    * @param e the Event that contains information about the received message
+    */
   private def onPM(e: PrivateMessageReceivedEvent): Unit = {
     val attachments = e.getMessage.getAttachments.asScala
     if (attachments.nonEmpty) {
-      attachments.find(a => a.getFileName.toLowerCase.endsWith(".xml")).foreach(xmlFile => {
+      attachments.filter(a => a.getFileName.toLowerCase.endsWith(".xml")).foreach(xmlFile => {
         saveConfig(xmlFile, e.getAuthor)
       })
       // language=RegExp
@@ -29,6 +46,14 @@ class ConfigManager(private val bot: VotebanBot) extends EventListener {
     }
   }
 
+  /**
+    * Called if the bot receives a config file in a PM
+    *
+    * Checks if the user is allowed to send that specific file and saves it
+    *
+    * @param xmlFile the attached config file
+    * @param author the user that did send the file
+    */
   private def saveConfig(xmlFile: Message.Attachment, author: User): Unit = {
     try {
       val config = bot.configService.readGuildConfig(xmlFile.getInputStream)
@@ -44,14 +69,45 @@ class ConfigManager(private val bot: VotebanBot) extends EventListener {
            }
          case None =>
            author.openPrivateChannel().complete()
-             .sendMessage("⚠ **Config file is for an unknown guild.**\nMaybe the guildId is wrong?").queue()
+             .sendMessage(s"⚠ **${xmlFile.getFileName} is for an unknown guild.**\nMaybe the guildId is wrong?").queue()
        }
     } catch {
       case e: Exception =>
         author.openPrivateChannel().complete()
-          .sendMessage(s"⚠ **Could not update that configuration** - ${e.getMessage}\nMaybe config file contains errors?").queue()
+          .sendMessage(s"⚠ **Could not upload ${xmlFile.getFileName}** - ${e.getMessage}\nMaybe config file contains errors?").queue()
     }
   }
 
-  private def sendConfig(author: User): Unit = ???
+  /**
+    * Called if `/config` command is received in a pm.
+    *
+    * Sends the config file for all known guilds where the user is admin
+    *
+    * @param author user that did send the message
+    */
+  private def sendConfig(author: User): Unit = {
+    val guilds = bot.JDA.getGuilds.asScala.filter(g => Option(g.getMember(author)) match {
+      case Some(member) if member.hasPermission(Permission.ADMINISTRATOR) => true
+      case _ => false
+    })
+    if (guilds.nonEmpty) {
+      guilds.foreach(g => {
+        try {
+          val output = new ByteArrayOutputStream()
+          bot.configService.writeGuildConfig(bot.configService.loadGuildConfig(g.getIdLong), output)
+          author.openPrivateChannel().complete().sendFile(
+            output.toByteArray,
+            XMLConfigurationService.guildConfigFile(g.getIdLong).getName,
+            new MessageBuilder(s"Config file for **${g.getName}:**").build()).queue()
+        } catch {
+          case e: Exception =>
+            author.openPrivateChannel().complete()
+              .sendMessage(s"⚠ **Could not send config file for ${g.getName}** _(${g.getId})_** - ${e.getMessage}\nCould be an internal error 🙈").queue()
+        }
+      })
+    } else {
+      author.openPrivateChannel().complete()
+        .sendMessage(s"⚠ **No guilds found where your are admin.**\nFor setting up the bot look at https://github.com/joblo2213/Voteban-t").queue()
+    }
+  }
 }
