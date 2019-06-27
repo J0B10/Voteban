b24a1ae2f65b8c1bd79211786bb96d84d5bd64fa
derNiklaas <derNiklaas@users.noreply.github.com>
2019-06-27 20:16:10 +0200

    Fix Typo in MybansCommand

--- src/main/scala/de/voteban/command/MybansCommand.scala
+++ src/main/scala/de/voteban/command/MybansCommand.scala
@@ -1,7 +1,7 @@
 package de.voteban.command
 import net.dv8tion.jda.core.entities.Message
 
-object MybannsCommand extends Command("mybanns", Seq()) {
+object MybansCommand extends Command("mybans", Seq()) {
 
   override protected def execute(message: Message): Unit = ??? //TODO
 }
