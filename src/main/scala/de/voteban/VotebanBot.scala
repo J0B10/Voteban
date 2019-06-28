9b1828b3b4587659651747dc933809be3081ad13
derNiklaas <derNiklaas@users.noreply.github.com>
2019-06-28 03:01:16 +0200

    [WIP] Added first implementation of the Voteban Command

--- src/main/scala/de/voteban/VotebanBot.scala
+++ src/main/scala/de/voteban/VotebanBot.scala
@@ -2,6 +2,7 @@ package de.voteban
 
 import de.voteban.config.XMLConfigurationService
 import de.voteban.db.JSONDatabaseService
+import de.voteban.command._
 import de.voteban.utils.{ConfigManager, RestartScheduler, WithLogger}
 import javax.security.auth.login.LoginException
 import net.dv8tion.jda.core.{JDA, JDABuilder}
@@ -37,11 +38,11 @@ class VotebanBot(private val apiToken: String, private val restartScheduler: Opt
     configService.loadCache()
     databaseService.loadDatabase()
     JDA addEventListener new ConfigManager(this)
-
+    JDA.addEventListener(VotebanCommand)
   }
 
 
   def onShutdown(): Unit = {
-
+    JDA.shutdown()
   }
 }
